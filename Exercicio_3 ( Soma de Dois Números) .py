46227ca1ae936ddc80d247a77e4c5bd5eec3eba1
Isaque M.V <isaquemvprofissional2000@gmail.com>
2025-09-24 17:42:47 -0300

    Create Exercicio_3 ( Soma de Dois Números) .py
    O script coleta dois números, que entram como string, e após coletá-los, os converte para inteiro, para que seja efetuada a soma.

---
+++ Exercicio_3 ( Soma de Dois Números) .py
@@ -0,0 +1,11 @@
+print('Olá! Digite dois números para serem somados.')
+
+numero_1 = input('Primeiro número: ')
+numero_1 = int(numero_1)
+
+numero_2 = input('Segundo número: ')
+numero_2 = int(numero_2)
+
+resultado = numero_1 + numero_2
+
+print(f'O resultado da soma entre {numero_1} e {numero_2} = {resultado}!')
