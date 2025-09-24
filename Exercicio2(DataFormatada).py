57d5e7115f851cf432c32cb3bc368f3a7fff52a1
Isaque M.V <isaquemvprofissional2000@gmail.com>
2025-09-24 17:33:22 -0300

    Create Exercicio2(DataFormatada).py
    O script pede ao usuário o dia, mês e ano de seu nascimento, e abaixo, mostra uma mensagem formatada com os dados coletados.

---
+++ Exercicio2(DataFormatada).py
@@ -0,0 +1,8 @@
+print('Seja, Bem-Vindo!\n' \
+'Por favor, digite a data de nascimento:')
+
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+
+print(f'Você nasceu em: {dia}/{mes}/{ano}')
