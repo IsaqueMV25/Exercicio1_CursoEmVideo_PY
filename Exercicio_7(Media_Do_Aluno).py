15d763fadc3e77e021cfa51a9bfcf7e586f39fce
Isaque M.V <isaquemvprofissional2000@gmail.com>
2025-09-24 18:41:17 -0300

    Create Exercicio_7(Media_Do_Aluno).py

---
+++ Exercicio_7(Media_Do_Aluno).py
@@ -0,0 +1,11 @@
+print('Mostrando a média de um aluno')
+nota_1 = input('Primeira nota: ')
+nota_1 = float(nota_1)
+
+nota_2 = input("Segunda nota: ")
+nota_2 = float(nota_2)
+
+media = (nota_1 + nota_2) / 2
+
+print(f'1º Nota: {nota_1} / 2° Nota: {nota_2}\n'
+      f'Media: {media}')
