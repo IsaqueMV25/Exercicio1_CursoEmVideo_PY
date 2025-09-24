f1fcc8f7e959e9c75edba49963c3877ea8b68511
Isaque M.V <isaquemvprofissional2000@gmail.com>
2025-09-24 18:43:02 -0300

    Create Exercicio_8(Conversao_De_Metros).py

---
+++ Exercicio_8(Conversao_De_Metros).py
@@ -0,0 +1,9 @@
+print('Conversão de Metros em cm e mm')
+entrada = input('Digite quantos metros quer converter: ')
+entrada = int(entrada)
+
+centimetros = entrada * 100 
+milimetros = entrada * 1000
+
+print(f'Centimetros: {centimetros}')
+print(f'Milímetros: {milimetros}')
